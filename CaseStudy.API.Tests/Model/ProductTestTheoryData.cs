8ea85f267ae45314a3875f12eb80cb42156ec56a
Ogün Çukur <ogun.cukur@rasyotek.com.tr>
2022-01-16 09:56:22 +0300

    added xunit tests for product proceses

---
+++ CaseStudy.API.Tests/Model/ProductTestTheoryData.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using CaseStudy.Application.Products.Commands.PostProduct;
+using CaseStudy.Application.Products.Commands.UpdateProduct;
+
+namespace CaseStudy.API.Tests.Model
+{
+    public class PostProductTestTheoryData : TheoryData<PostProductCommand>
+    {
+        public PostProductTestTheoryData()
+        {
+            Add(new PostProductCommand
+            {
+                Name = "Döner",
+                Description = "Türk Mutfağı",
+                CategoryId = "",
+                Currency = "TL",
+                Price = 25.90m
+            });
+
+            Add(new PostProductCommand
+            {
+                Name = "Kebap",
+                Description = "Türk Mutfağı",
+                CategoryId = "",
+                Currency = "TL",
+                Price = 40.50m
+            });
+        }
+    }
+
+    public class UpdateProductTestTheoryData : TheoryData<UpdateProductCommand>
+    {
+        public UpdateProductTestTheoryData()
+        {
+            Add(new UpdateProductCommand
+            {
+                Id = "61e2ff277334080a30012d24",
+                Name = "Döner",
+                Description = "Türk Mutfağı",
+                CategoryId = "",
+                Currency = "TL",
+                Price = 25.90m
+            });
+        }
+    }
+}
--- CaseStudy.API.Tests/ProductTests.cs
+++ CaseStudy.API.Tests/ProductTests.cs
@@ -1,9 +1,12 @@
+using CaseStudy.API.Tests.Model;
+using CaseStudy.Application.Products.Commands.PostProduct;
+using CaseStudy.Application.Products.Commands.UpdateProduct;
 using CaseStudy.Application.Products.Queries.GetProduct;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,13 +21,38 @@ namespace CaseStudy.API.Tests
             _client = fixture.CreateClient(new WebApplicationFactoryClientOptions { BaseAddress = new Uri("http://localhost:19450") });
         }
 
-        [Fact]
-        public async Task Get_ShouldReturnProduct_WhenTakesParameters()
+        [Theory, InlineData(new object[] { "61e2ff277334080a30012d24" })]
+        public async Task GetAsync_ShouldReturnProduct_WhenTakesId(string id)
         {
-            var id = "61e2ff277334080a30012d24";
             var response = await _client.GetAsync("api/v1/products" + "/" + id);
             var product = JsonConvert.DeserializeObject<ProductDto>(await response.Content.ReadAsStringAsync());
             Assert.IsType<ProductDto>(product);
+        }
+
+        [Theory, ClassData(typeof(PostProductTestTheoryData))]
+        public async Task PostAsync_ShouldReturnProduct_WhenTakesParameters(PostProductCommand parameter)
+        {
+            var json = JsonConvert.SerializeObject(parameter);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/v1/products", data);
+            var product = JsonConvert.DeserializeObject<ProductDto>(await response.Content.ReadAsStringAsync());
+            Assert.IsType<ProductDto>(product);
+        }
+
+        [Theory, ClassData(typeof(UpdateProductTestTheoryData))]
+        public async Task PutAsync_ShouldReturnProduct_WhenTakesParameters(UpdateProductCommand parameter)
+        {
+            var json = JsonConvert.SerializeObject(parameter);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync("api/v1/products", data);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory, InlineData(new object[] { "61e2ff277334080a30012d24" })]
+        public async Task DeleteAsync_ShouldReturnProduct_WhenTakesParameters(string id)
+        {
+            var response = await _client.DeleteAsync("api/v1/products" + "/" + id);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
